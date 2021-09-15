cd09deaf041024efac924d840867b035927d57b2
Prabhu <prabhu@Prabhus-MacBook-Pro.local>
2021-09-15 12:24:12 +0530

    Day07

---
+++ day07-web-app/src/main/java/com/jpmc/HelloServlet.java
@@ -0,0 +1,20 @@
+package com.jpmc;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDateTime;
+//http://localhost:8080/day07-web-app/hello
+@WebServlet(urlPatterns = {"/hello"})
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        LocalDateTime now = LocalDateTime.now();
+        PrintWriter out = resp.getWriter();
+        out.println("Hello! Time is " + now.toLocalTime());
+    }
+}
