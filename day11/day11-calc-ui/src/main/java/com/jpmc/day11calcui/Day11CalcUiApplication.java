d74101e2db0f983e8937278fa4332e58aeb7fab2
Prabhu <prabhu@Prabhus-MacBook-Pro.local>
2021-09-24 10:48:44 +0530

    Day11

---
+++ day11/day11-calc-ui/src/main/java/com/jpmc/day11calcui/Day11CalcUiApplication.java
@@ -0,0 +1,13 @@
+package com.jpmc.day11calcui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day11CalcUiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Day11CalcUiApplication.class, args);
+    }
+
+}
---
+++ day11/day11-calc-ui/src/test/java/com/jpmc/day11calcui/Day11CalcUiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpmc.day11calcui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day11CalcUiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
