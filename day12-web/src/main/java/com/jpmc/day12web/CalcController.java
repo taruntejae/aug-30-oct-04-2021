e2e10077c669778aa8ec3457d68a49c5d0d0a803
Prabhu <prabhu@Prabhus-MacBook-Pro.local>
2021-09-27 10:06:35 +0530

    Day12

--- day12-web/src/main/java/com/jpmc/day12web/CalcController.java
+++ day12-web/src/main/java/com/jpmc/day12web/CalcController.java
@@ -1,2 +1,30 @@
-package com.jpmc.day12web;public class CalcController {
+package com.jpmc.day12web;
+
+import io.swagger.annotations.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/calc")
+@Api(value="Calculator", tags = {"REST API for Calculator"})
+public class CalcController {
+
+    @ApiOperation(value = "Addition", notes = "Add two numbers")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "Sum"),
+            @ApiResponse(code = 500, message = "Oops!!!")
+    })
+    @GetMapping("/add/{a}/{b}")
+    public long add(@PathVariable @ApiParam(example = "12") long a,
+                    @ApiParam(example = "34") @PathVariable long b) {
+        return a + b;
+    }
+
+    @ApiOperation(value = "Subtraction", notes = "Subtract two numbers")
+    @GetMapping("/subtract/{a}/{b}")
+    public long subtract(@PathVariable long a, @PathVariable long b) {
+        return a - b;
+    }
 }
---
+++ day12-web/src/main/java/com/jpmc/day12web/Day12WebApplication.java
@@ -0,0 +1,13 @@
+package com.jpmc.day12web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day12WebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Day12WebApplication.class, args);
+    }
+
+}
