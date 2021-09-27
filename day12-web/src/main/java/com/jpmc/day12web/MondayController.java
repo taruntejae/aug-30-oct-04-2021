5d79e6b5b37a242d6927ef569d9fdd70279ffaac
Prabhu <prabhu@Prabhus-MacBook-Pro.local>
2021-09-27 10:35:24 +0530

    Day12

--- day12-web/src/main/java/com/jpmc/day12web/MondayController.java
+++ day12-web/src/main/java/com/jpmc/day12web/MondayController.java
@@ -3,6 +3,8 @@ package com.jpmc.day12web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Locale;
@@ -18,4 +20,18 @@ public class MondayController {
         return messageSource.getMessage("greet", null, locale);
         //return "Hello Monday!";
     }
+
+    @GetMapping("/bye/{name}")
+    public String sayBye(@PathVariable String name, Locale locale) {
+        return messageSource.getMessage("bye", new String[]{name}, locale);
+    }
+
+    @PostMapping("/withdraw/{amount}")
+    public String withdraw(@PathVariable int amount, Locale locale) {
+        if (amount < 1000) {
+            return messageSource.getMessage("error.invalid.amount", new String[]{amount+""}, locale);
+        }
+        return messageSource.getMessage("withdraw.success", null, locale);
+    }
+
 }
